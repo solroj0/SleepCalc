22f27be0cbfd08afa84ee848586365930e3c3b01
Sol Rojo <35583213+solroj0@users.noreply.github.com>
2024-02-29 15:03:37 -0800

    Update sleepcalculator.py

--- sleepcalculator.py
+++ sleepcalculator.py
@@ -1,7 +1,6 @@
 """
 This program is an attempt to correct a user's sleep schedule 
 The goal is to create a plan to adjust the sleeping times in a set period
-
 """
 from datetime import datetime, timedelta
 from colorama import Fore, Style
@@ -24,43 +23,43 @@ def find_efficient_direction(start, goal):
 
     # Compare absolute values
     if abs(added) < abs(subtract):
-        print("-- Adding hours is more efficient --\n"'-'*60)
+        print("-- Adding hours is more efficient --")
+        print("-" * 60)
         return "add", added
     else:
-        print("-- Subtracting hours is more efficient --\n"+'-'*60)
+        print("-- Subtracting hours is more efficient --")
+        print("-" * 60)
         return "subtract", subtract
 
-
 def create_schedule(operation, time_difference, time_interval):
-    schedule = [[f"day 0 ", start.strftime("%I:%M %p")]] #initiate list 
+    schedule = [[f"0", start.strftime("%I:%M %p")]] #initiate list 
     time_perday = time_difference / time_interval    
     time_step = start
     
     if operation == "add":
         for x in range(time_interval):
             time_step = time_step + time_perday
-            schedule.append([f"day {x+1} ", time_step.strftime("%I:%M %p")])
+            schedule.append([f"{x+1}", time_step.strftime("%I:%M %p")])
         return schedule
     elif operation == "subtract":
         for x in range(time_interval):
             time_step = time_step - time_perday
-            schedule.append([f"day {x+1} ", time_step.strftime("%I:%M %p")])
+            schedule.append([f"{x+1}", time_step.strftime("%I:%M %p")])
         return schedule
     
+def print_results(schedule):
+    print(f"{'  Day':<6}{'  Time':<12}")
+    for item in schedule:
+        print(f"{Fore.GREEN}|{Fore.YELLOW}{item[0]:>3}  {item[1]}{Fore.GREEN} |")    
+    print(Fore.GREEN + "-" *60 + Style.RESET_ALL)
+
 # init(autoreset=True) #to autoreset colors
 print(Fore.GREEN + "-"*60)
-print("Let's calculate the ideal path to a fixed circadian cycle")
+print("| Let's calculate the ideal path to a fixed circadian cycle ")
 start = get_time_input("| Current sleep time?: ")
 goal = get_time_input("| What time would you like to correct to?: ")
 time_interval = int(input("| How many days to correct?: "))
 
 operation, time_difference = find_efficient_direction(start, goal)
 schedule = create_schedule(operation, time_difference, time_interval)
-
-print(f"{'   Day':<12}{'Time':<12}")
-for item in schedule:
-    print(f"| {item[0]:<8} {item[1]} |")    
-print("-" *60 + Style.RESET_ALL)
-
-
- 
+print_results(schedule)
